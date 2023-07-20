7891f5103b33026be82fbf3ffecb0d1c68907b46
jsantis <san21153@uvg.edu.gt>
2023-07-19 22:16:48 -0600

    correcion de errores y visualización de modelo 3D

--- pruebaOBJ/main.py
+++ pruebaOBJ/main.py
@@ -10,12 +10,10 @@ rend = Renderer(width,height)
 rend.glClearColor(0, 0, 0)
 rend.glClear()
 
-rend.glModelMatrix(translate = (width/2, height/2,0))
-
 rend.vertexShader = shaders.vertexShader
 rend.fragmentShader = shaders.fragmentShader
 
-rend.glLoadModel("Dragon.obj", translate = (width/2, height/2,0), scale = (5, 5,0), rotate = (0, 0, 0))
+rend.glLoadModel("Dragon.obj", translate = (32, 25,0), scale = (30, 30,0), rotate = (0, 5, 0))
 
 rend.glRender()
 
